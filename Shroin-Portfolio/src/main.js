f2a1ca9a57d116df001cbe1ee8046823a140bec7
Jayanth Srinivasan <81894141+Jayanth-Srinivasan@users.noreply.github.com>
2021-08-04 21:15:23 +0530

    Initial

---
+++ Shroin-Portfolio/src/main.js
@@ -0,0 +1,44 @@
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+
+function scrollTop() {
+    const scrolltop = document.getElementById('scroll-top')
+    if(this.scrollY>=560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+
+}
+window.addEventListener('scroll',scrollTop)
+gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
+
+
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
+
+
+gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
+gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
+
+
+gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 3.2, y: 25, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
+
+gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
+
+gsap.from('.about__img', {opacity: 0, duration: 2, delay: 4.5, x: 60})
+
+// gsap.from('.about__info', {opacity: 0, duration: 3, delay: 6, y: 25})
+// gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 6, y: 25, ease:'expo.out', stagger: .3})
+
+
